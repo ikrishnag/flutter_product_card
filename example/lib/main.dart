d73d35b4feefc8b4a8a7b630d2ecee6588d09ea9
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-10 19:17:52 +0530

    main.dart modified

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: SizedBox(
@@ -22,13 +22,23 @@ class MainApp extends StatelessWidget {
                   imageUrl:
                       'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQndSK7hvssofrM2uzv75NxVjrkAwH3RwyqWcBesUsmq1ipmkuljRr6x_SRbCKaBXvjTR9CKfAaEFtmUFw-69o52wgVMgk2hp8KDYr4FvKtQ8ZfKewgOW4gDQ&usqp=CAE',
                   categoryName: 'Pants',
-                  shortDescription: 'Comfortable & airy. ',
-                  price: 35,
-                  quantity: 3,
                   productName: 'Men Linen Pants',
-                  isAvailable: true,
-                  rating:4.2,
-                  discountPercentage: 50,
+                  price: 199.99,
+                  currency: '\$', // Default is '$'
+                  onTap: () {
+                    // Handle card tap event
+                  },
+                  onFavoritePressed: () {
+                    // Handle favorite button press
+                  },
+                  shortDescription:
+                      'comfortable & airy.', // Optional short description
+                  rating: 4.2, // Optional rating
+                  discountPercentage: 35.0, // Optional discount percentage
+                  isAvailable: true, // Optional availability status
+                  cardColor: Colors.white, // Optional card background color
+                  textColor: Colors.black, // Optional text color
+                  borderRadius: 8.0, // Optional border radius
                 ),
               ],
             ),
