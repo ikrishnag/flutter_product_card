3614c1b0e85a30d9de866276af193c23d2d9eea7
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-10 21:34:37 +0530

    main.dart modified

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,31 +16,27 @@ class MainApp extends StatelessWidget {
           child: SizedBox(
             height: 400,
             width: 300,
-            child: Column(
-              children: [
-                ProductCard(
-                  imageUrl:
-                      'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQndSK7hvssofrM2uzv75NxVjrkAwH3RwyqWcBesUsmq1ipmkuljRr6x_SRbCKaBXvjTR9CKfAaEFtmUFw-69o52wgVMgk2hp8KDYr4FvKtQ8ZfKewgOW4gDQ&usqp=CAE',
-                  categoryName: 'Pants',
-                  productName: 'Men Linen Pants',
-                  price: 199.99,
-                  currency: '\$', // Default is '$'
-                  onTap: () {
-                    // Handle card tap event
-                  },
-                  onFavoritePressed: () {
-                    // Handle favorite button press
-                  },
-                  shortDescription:
-                      'comfortable & airy.', // Optional short description
-                  rating: 4.2, // Optional rating
-                  discountPercentage: 35.0, // Optional discount percentage
-                  isAvailable: true, // Optional availability status
-                  cardColor: Colors.white, // Optional card background color
-                  textColor: Colors.black, // Optional text color
-                  borderRadius: 8.0, // Optional border radius
-                ),
-              ],
+            child: ProductCard(
+              imageUrl:
+                  'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQndSK7hvssofrM2uzv75NxVjrkAwH3RwyqWcBesUsmq1ipmkuljRr6x_SRbCKaBXvjTR9CKfAaEFtmUFw-69o52wgVMgk2hp8KDYr4FvKtQ8ZfKewgOW4gDQ&usqp=CAE',
+              categoryName: 'Pants',
+              productName: 'Men Linen Pants',
+              price: 199.99,
+              currency: '\$', // Default is '$'
+              onTap: () {
+                // Handle card tap event
+              },
+              onFavoritePressed: () {
+                // Handle favorite button press
+              },
+              shortDescription:
+                  'comfortable & airy.', // Optional short description
+              rating: 4.2, // Optional rating
+              discountPercentage: 35.0, // Optional discount percentage
+              isAvailable: true, // Optional availability status
+              cardColor: Colors.white, // Optional card background color
+              textColor: Colors.black, // Optional text color
+              borderRadius: 8.0, // Optional border radius
             ),
           ),
         ),
