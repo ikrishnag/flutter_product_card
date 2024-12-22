392a4a0690c9cf527509c86db92a401793b93331
ikrishnag <e.krishnagupta@gmail.com>
2024-12-22 16:35:41 +0530

    fix: update layout of ProductCard in MainApp for better alignment and dimensions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,33 +12,36 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: SizedBox(
-            height: 400,
-            width: 300,
-            child: ProductCard(
-              imageUrl:
-                  'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQndSK7hvssofrM2uzv75NxVjrkAwH3RwyqWcBesUsmq1ipmkuljRr6x_SRbCKaBXvjTR9CKfAaEFtmUFw-69o52wgVMgk2hp8KDYr4FvKtQ8ZfKewgOW4gDQ&usqp=CAE',
-              categoryName: 'Pants',
-              productName: 'Men Linen Pants',
-              price: 199.99,
-              currency: '\$', // Default is '$'
-              onTap: () {
-                // Handle card tap event
-              },
-              onFavoritePressed: () {
-                // Handle favorite button press
-              },
-              shortDescription:
-                  'comfortable & airy.', // Optional short description
-              rating: 4.2, // Optional rating
-              discountPercentage: 35.0, // Optional discount percentage
-              isAvailable: true, // Optional availability status
-              cardColor: Colors.white, // Optional card background color
-              textColor: Colors.black, // Optional text color
-              borderRadius: 8.0, // Optional border radius
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: ProductCard(
+                imageUrl:
+                    'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQndSK7hvssofrM2uzv75NxVjrkAwH3RwyqWcBesUsmq1ipmkuljRr6x_SRbCKaBXvjTR9CKfAaEFtmUFw-69o52wgVMgk2hp8KDYr4FvKtQ8ZfKewgOW4gDQ&usqp=CAE',
+                categoryName: 'Pants',
+                productName: 'Men Linen Pants',
+                price: 199.99,
+                currency: '\$', // Default is '$'
+                onTap: () {
+                  // Handle card tap event
+                },
+                onFavoritePressed: () {
+                  // Handle favorite button press
+                },
+                shortDescription:
+                    'comfortable & airy.', // Optional short description
+                rating: 4.2, // Optional rating
+                discountPercentage: 35.0, // Optional discount percentage
+                isAvailable: true, // Optional availability status
+                cardColor: Colors.white, // Optional card background color
+                textColor: Colors.black, // Optional text color
+                borderRadius: 8.0, // Optional border radius
+                width: 300, // optional
+                height: 380, // optional
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
