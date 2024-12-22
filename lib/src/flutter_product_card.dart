1f008f6fc112e5292a96b2002f3ede52d90da544
ikrishnag <e.krishnagupta@gmail.com>
2024-12-22 16:08:02 +0530

    fix: adjust padding for short description in ProductCard for better layout

--- lib/src/flutter_product_card.dart
+++ lib/src/flutter_product_card.dart
@@ -162,7 +162,7 @@ class ProductCardState extends State<ProductCard> {
                   // Short description (if provided)
                   if (widget.shortDescription!.isNotEmpty)
                     Padding(
-                      padding: const EdgeInsets.only(top: 6.0),
+                      padding: const EdgeInsets.only(top: 4.0),
                       child: Text(
                         widget.shortDescription!,
                         style: TextStyle(
